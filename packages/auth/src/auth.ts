214893c6f4a09e2bde0e2cdcf54a41b1909e1f94
ashutosh-k-root <ashutosh.dev.root@gmail.com>
2026-02-22 19:31:45 +0530

    Better Auth Upadted Docs and added auth to talkspace

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,11 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
-import {prisma} from "@workspace/database";
+
+import { prisma } from "@workspace/database";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", // or "mysql", "sqlite", ...etc
     }),
     emailAndPassword: { 
     enabled: true, 
