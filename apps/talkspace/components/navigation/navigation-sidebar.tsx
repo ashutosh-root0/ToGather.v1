076f53fa510ccb4967bbee835875db4424b9187b
ashutosh-k-root <ashutosh.dev.root@gmail.com>
2026-03-02 16:39:03 +0530

    Added Navigation Sidebar Items

--- apps/talkspace/components/navigation/navigation-sidebar.tsx
+++ apps/talkspace/components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,10 @@ import { prisma } from "@workspace/database";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
 
+import {Separator} from "@workspace/ui/components/separator"
+import { ScrollArea } from "@workspace/ui/components/scroll-area";
+import { NavigationItem } from "./navigation-item";
+
 export const NavigationSidebar = async() =>{
 
     const profile = await currentProfile();
@@ -23,6 +27,19 @@ export const NavigationSidebar = async() =>{
     return(
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
             <NavigationAction/>
+            <Separator 
+            className= "h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem
+                            id={server.id}
+                            imageUrl={server.imageUrl}
+                            name={server.name}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
         </div>
-    )
+    )   
 }
